0cfe7080e13c0473136992f379970198d261e00c
chengjia feng <feng@chengjias-MacBook-Air.local>
2024-02-18 20:46:05 -0500

    updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,8 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const newDate = new Date();
+  const date = newDate.getDate();
+  const month = newDate.getMonth()+1;
+  const year = newDate.getFullYear();
+  const date1 = new Date(2023, 2, 16);
+  // const date1tostring = date1.toString();
+  const oneDay = 24 * 60 * 60 * 1000;
+  const today = `${month}/${date}/${year}`
+  const countdate= Math.round(Math.abs((newDate - date1) / oneDay));
+  const [state, setState] = useState(false)
   return (
     <>
      
@@ -17,8 +26,9 @@ function App() {
           </div>
           <p>Gong</p>
          </div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setState((!state))}>
+          <p id='countText'>{!state? 'Today is':"It's been"}</p>
+          {state? countdate+" days": today} 
         </button>
       
       
